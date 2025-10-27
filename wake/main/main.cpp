306c06d62b8b2708249595624a5be85940b1480b
smart-life-tech <christlightworld@gmail.com>
2025-10-27 14:07:07 +0100

    "Removed #include directive for esp_sr_iface.h"

--- wake/main/main.cpp
+++ wake/main/main.cpp
@@ -2,7 +2,7 @@
 #include "esp_system.h"
 #include "esp_wn_iface.h"
 #include "esp_wn_models.h"
-#include "esp_sr_iface.h"
+//#include "esp_sr_iface.h"
 #include "driver/i2s_std.h"
 #include "driver/gpio.h"
 
