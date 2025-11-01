64f0538682f314627c6a49e8eeb7b58373516b30
smart-life-tech <christlightworld@gmail.com>
2025-11-01 13:11:07 +0100

    `Commented out esp_afe_sr_set_wakenet and esp_afe_sr_set_wakenet_sensitivity calls in app_main function.`

--- wake/main/main.cpp
+++ wake/main/main.cpp
@@ -261,10 +261,10 @@ extern "C" void app_main()
     }
     // modify wakenet detection threshold
     // Select model by index (0 = wn9_alexa)
-    esp_afe_sr_set_wakenet(afe_handle, afe_data, 0);
+    //esp_afe_sr_set_wakenet(afe_handle, afe_data, 0);
 
     // Lower sensitivity = easier detection (0.0 = hard, 1.0 = easy)
-    esp_afe_sr_set_wakenet_sensitivity(afe_handle, 0.7f); // 0.7 = balanced
+    //esp_afe_sr_set_wakenet_sensitivity(afe_handle, 0.7f); // 0.7 = balanced
     // afe_handle->set_wakenet_threshold(afe_data, 2, 0.6); // set model2's threshold to 0.6
     // afe_handle->reset_wakenet_threshold(afe_data, 1);    // reset model1's threshold to default
     // afe_handle->reset_wakenet_threshold(afe_data, 2);    // reset model2's threshold to default
